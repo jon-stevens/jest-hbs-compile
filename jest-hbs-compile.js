923d15f141001e299aef3e19bdf8595e9720d0bd
jon-stevens <j@jsteve.uk>
2023-12-15 10:15:40 +0000

    Add main file

---
+++ jest-hbs-compile.js
@@ -0,0 +1,47 @@
+import path from 'node:path';
+import {readdirSync, statSync, readFileSync} from 'node:fs';
+import Handlebars from 'handlebars';
+import handlebarsHelpers from './handlebars-helpers';
+
+class TestHelpers {
+	constructor(page) {
+		this.page = page;
+		this.partialsDirectory = '/handlebars/partials';
+		this.fullPartialsDirectory = './[project]/handlebars/partials';
+
+		Handlebars.registerHelper(handlebarsHelpers);
+
+		this.findAllPartials();
+	}
+
+	async compileTemplate(config) {
+		return new Promise((resolve, reject) => {
+			try {
+				const {mainTemplate, model} = config;
+				const html = mainTemplate(model);
+				resolve(html);
+			} catch (error) {
+				reject(error);
+			}
+		});
+	}
+
+	findAllPartials(partialsPath) {
+		let directory = partialsPath || this.fullPartialsDirectory;
+		let partials = readdirSync(directory);
+
+		for (let file of partials) {
+			if (statSync(directory + '/' + file).isDirectory()) {
+				this.findAllPartials(directory + '/' + file);
+			} else {
+				const name = path.parse(file);
+				Handlebars.registerPartial(
+					`${directory.replace('./src/main/resources/', '')}/${name.name}`,
+					readFileSync(path.join(directory + '/' + file), 'utf-8')
+				);
+			}
+		}
+	}
+}
+
+export default TestHelpers;
